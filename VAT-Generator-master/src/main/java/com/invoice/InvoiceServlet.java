60d79864df6d85c79c725485aec895d9317d1e81
Cia6 <49695836+Cia6@users.noreply.github.com>
2019-06-21 12:53:07 +0200

    Add files via upload

---
+++ VAT-Generator-master/src/main/java/com/invoice/InvoiceServlet.java
@@ -0,0 +1,81 @@
+package com.invoice;
+
+import com.invoice.data.DBController;
+import com.invoice.data.Product;
+import org.apache.pdfbox.pdmodel.PDDocument;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet(name = "InvoiceServlet", urlPatterns = {"invoice"}, loadOnStartup = 1)
+public class InvoiceServlet extends HttpServlet {
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+
+        Invoice invoice = getInvoiceFromRequest(request);
+
+        Product product = new Product("", "", ""); // Creating new product with sent specification.
+
+        DBController databaseController = new DBController();
+
+        databaseController.addProductToDatabase(product); // Adding new product to database.
+
+        List<Product> productList = new ArrayList<>(); // Creating empty list of products.
+        productList.addAll(databaseController.getProductsList()); // Adding all products from database to created list.
+
+        ByteArrayOutputStream byteArrayOutputStream = new InvoiceCreator().create(invoice); // Creating PDF with list of products and saving it as output.
+
+        response.setContentType("application/pdf");
+        response.addHeader("Content-Disposition", "attachment; filename=" + "Invoice-" + "" + ".pdf");
+        response.setContentLength(byteArrayOutputStream.size());
+
+        try {
+            ServletOutputStream servletOutputStream = response.getOutputStream();
+            byteArrayOutputStream.writeTo(servletOutputStream);
+            byteArrayOutputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public Invoice getInvoiceFromRequest(HttpServletRequest request){
+        Invoice invoice = new Invoice();
+        invoice.setFaktura(request.getParameter("faktura"));
+        invoice.setNumerFaktury(request.getParameter("numer-faktury"));
+        invoice.setDataWFaktury(request.getParameter("data-w-faktury"));
+        invoice.setMiejsceFaktury(request.getParameter("miejsce-faktury"));
+
+        invoice.setNameS(request.getParameter("name-s"));
+        invoice.setAdresS(request.getParameter("adres-s"));
+        invoice.setAdresS2(request.getParameter("adres-s2"));
+        invoice.setNIPS(request.getParameter("NIP-s"));
+        invoice.setREGONS(request.getParameter("REGON-s"));
+        invoice.setDodatekS(request.getParameter("dodatek-s"));
+
+        invoice.setWaluta(request.getParameter("waluta"));
+
+        invoice.setNettoPrice(request.getParameter("netto-price"));
+        invoice.setVat(request.getParameter("vat"));
+
+        invoice.setNameN(request.getParameter("name-n"));
+        invoice.setAdresN(request.getParameter("adres-n"));
+        invoice.setAdresN2(request.getParameter("adres-n2"));
+        invoice.setNIPN(request.getParameter("NIP-n"));
+        invoice.setREGONN(request.getParameter("REGON-n"));
+        invoice.setDodatekN(request.getParameter("dodatek-n"));
+
+        return invoice;
+    }
+
+}
